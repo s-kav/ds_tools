27318e2c8e282431de609e23101116402504bd53
Sergii Kavun <62832866+s-kav@users.noreply.github.com>
2025-07-11 20:11:12 +0300

    .

--- src/ds_tool/__init__.py
+++ src/ds_tool/__init__.py
@@ -3,6 +3,6 @@
 DSTools: A library of helpful functions for various data science research stages.
 """
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 from .ds_tool import DSTools, MetricsConfig, CorrelationConfig, OutlierConfig, DistributionConfig, GrubbsTestResult
